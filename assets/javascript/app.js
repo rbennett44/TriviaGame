1439995bb4e5e7cc8794314ccbe437d4fcce666c
Richard <richardbennett44@gmail.com>
2018-09-11 07:37:07 -0600

    update1

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+//This is for the Canadian Trivia game
+//Have a start button that will start the game
+
+var gametime = setTimeout(thirtyseconds, 1000 * 30);
+//Set countdown timer to 30 seconds
+$("#startGame").on("click", function());
+//10 questions and their answers
+var question1 = $("#question1")[
+    "Pierre Trudeau": false,
+    "Kim Campbell": false,
+    "Justin Trudea": true,
+    "Brian Mulroney": false
+];
+var question2 = [
+    "Ottawa": true,
+    "Montreal": false,
+    "Toronto": false,
+    "Winnipeg": false
+]
+var question3 = [
+    "Neepawa": false,
+    "Winnipeg": true,
+    "Montreal": false,
+    "Vancouver": false
+]
+var question4 = [
+    "Most of the time": false,
+    "Absolutely": false,
+    "Depends on how close they are to a hockey game": false,
+    "All of the above": true
+]
+var question5 = [
+    
+]
+
+//have a way to tell which answer is the right one
+//game has to be abel to reset
+//have to keep track of which ones you got correct and which ones you got wrong
+// post right and wrong answers 
